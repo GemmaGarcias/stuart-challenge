12b0b9ac37aa5e5c0b688cdd9f6b9f2a722cb013
Gemma Garcias <gemma.garcias.e@gmail.com>
2020-06-21 17:03:30 +0200

    first approach, addresses form and css loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,21 @@
-import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
-
-function Map() {
-  return (
-    <GoogleMap 
-        defaultZoom={10}
-        defaultCenter={{ lat: 45.421532, lng: -75.697189}}
-    />
-  )
-}
-
-const WrappedMap = withScriptjs(withGoogleMap(Map))
+import React from 'react';
+import { WrappedMap } from './map/Map';
+import Addresses from './form/Addresses';
+import './App.css';
 
 export default function App() {
   return (
-    <div style={{width: "50vw", height: "50vh"}}>
+    <React.Fragment>
+    <div style={{width: "50vw", height: "100vh"}}>
+      <Addresses />
       <WrappedMap 
         isMarkerShown 
         googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyD42XFQV9gGgBclEgi2z3B-SsjAgrw55Jk&callback=initMap'} 
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
-        />
-        
+        />   
     </div>
+    </React.Fragment>
   );
 }
---
+++ src/components/form/Addresses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Addresses.css';
+
+export default function Addresses() {
+  return (
+    <div className="container">
+      hello world
+      <form>
+        <input/>
+        <input/>
+      </form>
+    </div>
+  );
+}
