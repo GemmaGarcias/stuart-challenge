6b7b4acaaabbd02fce3343bc7c6ac4faadeacd7a
Gemma Garcias <gemma.garcias.e@gmail.com>
2020-06-21 19:12:28 +0200

    styles in container

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,16 @@ import './App.css';
 export default function App() {
   return (
     <React.Fragment>
-    <div style={{width: "50vw", height: "100vh"}}>
-      <Addresses />
-      <WrappedMap 
-        isMarkerShown 
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyD42XFQV9gGgBclEgi2z3B-SsjAgrw55Jk&callback=initMap'} 
-        loadingElement={<div style={{ height: "100%" }} />}
-        containerElement={<div style={{ height: "100%" }} />}
-        mapElement={<div style={{ height: "100%" }} />}
-        />   
-    </div>
+      <div style={{width: "60vw", height: "90vh"}}>
+        <Addresses />
+        <WrappedMap 
+          isMarkerShown 
+          googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyD42XFQV9gGgBclEgi2z3B-SsjAgrw55Jk&callback=initMap'} 
+          loadingElement={<div style={{ height: "100%" }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+          />   
+      </div>
     </React.Fragment>
   );
 }
--- src/components/form/Addresses.js
+++ src/components/form/Addresses.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import './Addresses.css';
+import pickUpBadgeBlank from '../../assets/pickUpBadgeBlank.svg';
+import dropOffBadgeBlank from '../../assets/dropOffBadgeBlank.svg';
 
 export default function Addresses() {
+
   return (
     <div className="container">
-      hello world
       <form>
-        <input/>
-        <input/>
+        <label className="label">
+          <img src={pickUpBadgeBlank} className="imgIcon"/>
+          <input type="text" name="pickUp" className="input" placeholder="Pick up address"/>
+        </label>
+        <label className="label">
+          <img src={dropOffBadgeBlank} className="imgIcon"/>
+          <input type="text" name="dropOff" className="input" placeholder="Dropp off address"/>
+        </label>
+        <input type="submit" value="Create job" className="button" />
       </form>
     </div>
   );
