c0b398fb87b8eec43cfb0341170441f1a9b5738c
Gemma Garcias <gemma.garcias.e@gmail.com>
2020-06-21 15:40:25 +0200

    display google map

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-export default class App extends React.Component {
+function Map() {
+  return (
+    <GoogleMap 
+        defaultZoom={10}
+        defaultCenter={{ lat: 45.421532, lng: -75.697189}}
+    />
+  )
+}
 
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+export default function App() {
+  return (
+    <div style={{width: "50vw", height: "50vh"}}>
+      <WrappedMap 
+        isMarkerShown 
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyD42XFQV9gGgBclEgi2z3B-SsjAgrw55Jk&callback=initMap'} 
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+        />
+        
+    </div>
+  );
 }
